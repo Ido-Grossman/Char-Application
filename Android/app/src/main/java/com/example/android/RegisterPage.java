84e8500da539c892a6ce497c0bc99f6208303c25
santisz7897 <santisz7897@gmail.com>
2022-06-07 23:31:55 +0300

    click here now redirects to register page and increased the size of title

---
+++ Android/app/src/main/java/com/example/android/RegisterPage.java
@@ -0,0 +1,16 @@
+package com.example.android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_page);
+    }
+}
