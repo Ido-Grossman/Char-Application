319fdb51cfdd2a24ef667aac59cea578c60383f5
EliZilber <elisilverstine@gmail.com>
2022-06-07 18:41:51 +0300

    added buttons to chat page

--- Android/app/src/main/java/com/example/android/ChatActivity.java
+++ Android/app/src/main/java/com/example/android/ChatActivity.java
@@ -3,6 +3,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,6 +14,11 @@ public class ChatActivity extends AppCompatActivity{
         ImageView profilePictureView;
         TextView userNameView;
 
+        void createPageButtons(){
+            ImageButton backButton = findViewById(R.id.back_button);
+            Intent intent = new Intent(getApplicationContext(), ListActivity.class);
+            backButton.setOnClickListener(view -> startActivity(intent));
+        }
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -23,10 +31,12 @@ public class ChatActivity extends AppCompatActivity{
 
             if (activityIntent != null) {
                 String userName = activityIntent.getStringExtra("userName");
-                //todo - change
+                //todo - support image
              /*   int profilePicture = activityIntent.getIntExtra("profilePicture", R.drawable.blue);
                 profilePictureView.setImageResource(profilePicture);*/
                 userNameView.setText(userName);
             }
+
+            createPageButtons();
     }
 }
