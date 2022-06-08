23cdd561acf8f36c77910aa0150d61544a99deed
idodd <idoddii@gmail.com>
2022-06-08 19:13:22 +0300

    Register light mode done

--- Android/app/src/main/java/com/example/android/RegisterPage.java
+++ Android/app/src/main/java/com/example/android/RegisterPage.java
@@ -2,8 +2,10 @@ package com.example.android;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +14,18 @@ public class RegisterPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_page);
+
+        ImageButton UploadImg = findViewById(R.id.UploadImg);
+        UploadImg.setOnClickListener(view -> {
+            Intent takePicture = new Intent(Intent.ACTION_PICK,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivity(takePicture);
+        });
+        Button registerBtn = findViewById(R.id.RegisterBtn);
+        registerBtn.setOnClickListener(view -> {
+            EditText username = findViewById(R.id.RegisterUserName);
+            EditText password = findViewById(R.id.RegisterPassword1);
+        });
+
     }
 }
